0d3599f88ea0ada1e019002f2372ce6709f4f647
Francisco Bennassar <fbajo97@gmail.com>
2024-07-18 14:26:38 -0400

    primer commit desde rama francisco/JFrameInicial

--- src/main/java/com/mycompany/inmuebles/App.java
+++ src/main/java/com/mycompany/inmuebles/App.java
@@ -4,6 +4,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	System.out.println( "Hello World!" );
+    	System.out.println( "Hello World! rama nueva" );
+
     }
 }
