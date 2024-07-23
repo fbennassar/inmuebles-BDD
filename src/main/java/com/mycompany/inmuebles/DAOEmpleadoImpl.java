1f5050a315c8c0453e1e5aa0c5b291051dd8ae0b
Francisco Bennassar <fbajo97@gmail.com>
2024-07-23 00:11:55 -0400

    Editar y buscar por nombre listo

--- src/main/java/com/mycompany/inmuebles/DAOEmpleadoImpl.java
+++ src/main/java/com/mycompany/inmuebles/DAOEmpleadoImpl.java
@@ -33,8 +33,23 @@ public class DAOEmpleadoImpl extends Database implements DAOEmpleado {
 	}
 
 	@Override
-	public void actualizar(Empleado empleado) {
-		// TODO Auto-generated method stub
+	public void actualizar(Empleado empleado) throws Exception {
+
+		try {
+			this.Conectar();
+			PreparedStatement ps = this.conexion.prepareStatement(
+					"UPDATE empleado SET nombre = ?, telefono = ?, email = ?, cedula = ? WHERE idEmpleado = ?;");
+			ps.setString(1, empleado.getNombre());
+			ps.setString(2, empleado.getTelefono());
+			ps.setString(3, empleado.getEmail());
+			ps.setInt(4, empleado.getCedula());
+			ps.setInt(5, empleado.getId());
+			ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.Cerrar();
+		}
 		
 	}
 
@@ -58,6 +73,37 @@ public class DAOEmpleadoImpl extends Database implements DAOEmpleado {
 	public Empleado buscarPorCedula(int cedula) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	@Override
+	public List<Empleado> buscarPorNombre(String nombre) throws Exception {
+		List<Empleado> empleados = null;
+		
+		try {
+			this.Conectar();
+			String query = nombre.isEmpty() ? "SELECT * FROM empleado;" : "SELECT * FROM empleado WHERE nombre LIKE '%" + nombre + "%';";
+			PreparedStatement ps = this.conexion.prepareStatement(query);
+			
+			empleados = new ArrayList<Empleado>();
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+                Empleado empleado = new Empleado();
+                empleado.setId(rs.getInt("idEmpleado"));
+                empleado.setNombre(rs.getString("nombre"));
+                empleado.setTelefono(rs.getString("telefono"));
+                empleado.setEmail(rs.getString("email"));
+                empleado.setCedula(rs.getInt("cedula"));
+                
+                empleados.add(empleado);
+            }
+			rs.close();
+			ps.close();
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			this.Cerrar();
+		}
+		return empleados;
 	}
 
 	@Override
@@ -91,5 +137,34 @@ public class DAOEmpleadoImpl extends Database implements DAOEmpleado {
 		
 		return empleados;
 	}
+
+	@Override
+	public Empleado buscarPorId(int idEmpleado) throws Exception {
+		Empleado empleado = new Empleado();
+		
+		try {
+			this.Conectar();
+			PreparedStatement ps = this.conexion.prepareStatement("SELECT * FROM empleado WHERE idEmpleado = ?;");
+			ps.setInt(1, idEmpleado);
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				empleado.setId(rs.getInt("idEmpleado"));
+				empleado.setNombre(rs.getString("nombre"));
+				empleado.setTelefono(rs.getString("telefono"));
+				empleado.setEmail(rs.getString("email"));
+				empleado.setCedula(rs.getInt("cedula"));
+			}
+			rs.close();
+			ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			this.Cerrar();
+		}
+		return empleado;
+	}
+
+
 
 }
